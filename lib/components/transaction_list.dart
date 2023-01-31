a49b26667bd7c003711a317fb01679bd02f6d072
alexcstd <alex.scustod@gmail.com>
2023-01-31 14:35:49 -0300

    adicionando listview para melhor desempenho do app

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -9,49 +9,44 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            ...transactions!.map((transacao) {
-              return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10,
-                      ),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.purple, width: 2)),
-                      child: Text(
-                        "R\$ " + transacao.value!.toString(),
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple),
-                      ),
-                      padding: EdgeInsets.all(10),
+      child: ListView.builder(itemCount: transactions!.length, itemBuilder: ((context, index) {
+        return Card(
+              child: Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 10,
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          transacao.title!,
-                          style:
-                              TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                        ),
-                        Text(
-                          DateFormat("d MMM y").format(transacao.date!),
-                          style: TextStyle(color: Colors.grey),
-                        )
-                      ],
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple, width: 2)),
+                    child: Text(
+                      "R\$ " + transactions![index].value!.toString(),
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple),
                     ),
-                  ],
-                ),
-              );
-            }).toList(),
-          ],
-        ),
+                    padding: EdgeInsets.all(10),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        transactions![index].title!,
+                        style:
+                            TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                      Text(
+                        DateFormat("d MMM y").format(transactions![index].date!),
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            );
+      }),
       ),
     );
   }
